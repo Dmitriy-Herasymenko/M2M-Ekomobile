e84e37cfbbf41c49a6d4b7e7e70584e824d67d38
DmitriyHerasymenko <dmitriy.herasymenko@482.solutions>
2021-11-03 13:05:44 +0200

    create store, reducer: cashReducers & customerReducers, and install redux-devtools-extension

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import {useDispatch, useSelector} from "react-redux";
+
+
 
 export const App = () => {
+    const dispatch = useDispatch();
+    const cash = useSelector(state => state.cash.cash);
+
+
+    const addCash = cash => dispatch({type: "ADD_CASH", payload: cash });
+    const getCash = cash => dispatch({type: "GET_CASH", payload: cash });
+
+
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+      <div className="App">
+        <div>{cash}</div>
+          <div style={{display: 'flex'}}>
+              <button onClick={() => addCash(Number(prompt()))}>ADD CASH</button>
+              <button onClick={() => getCash(Number(prompt()))}>GET CASH</button>
+          </div>
+      </div>
   );
 };
